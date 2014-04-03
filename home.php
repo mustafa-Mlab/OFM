547ecf84fde4e09f36a498fa946bb85370a92688
nisan92 <nisan92@yahoo.com>
2014-04-04 00:12:53 +0600

    9th commit
    multiple login disabled

--- home.php
+++ home.php
@@ -1,6 +1,11 @@
 <div class="container">
-            <div class="row ofwhite">
-                <div class="col-md-6"><?php require 'newsscroll.php';?></div>
-                <div class="col-md-6"> <?php require './forms/login-form.php';?></div>
-            </div>
-        </div>
+    <?php 
+        if(isset($_SESSION['id'])){
+            header("location:logout.php");
+        }
+    ?>
+    <div class="row ofwhite">
+        <div class="col-md-6"><?php require 'newsscroll.php'; ?></div>
+        <div class="col-md-6"> <?php require './forms/login-form.php'; ?></div>
+    </div>
+</div>
