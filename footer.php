eb1fbd626c6f47cb0ff7a40528a0ae5886f474f6
nisan92 <nisan92@yahoo.com>
2014-05-06 09:19:54 +0600

    24th commit
    footer fixed
    some process need to fix (change database name)

--- footer.php
+++ footer.php
@@ -1,13 +1,18 @@
 <div class="footer container clearfix ofwhite">
     <div class="row">
-        <div class="col-md-4">
-            @copyright MasterMind 
+        <div class="col-md-3">
+            
         </div>
-        <div class="col-md-4">
-            01724939534
+        <div class="col-md-6 center-align">
+           <h5>It's an academic project done by </br>
+            Md Mustafa Kamal Hossain (102-15-1017)</br>
+            Tashfia Yasmine Anika (102-15-1003)</br>
+            And Md Faizur Rahman </br>
+            26th batch of Computer Science and Engineering Department  </br>
+            Daffodil International University </br> </h5>
         </div>
-        <div class="col-md-4">
-
+        <div class="col-md-3">
+            
         </div>
     </div>
 </div>        <script src="js/jquery.js"></script>
